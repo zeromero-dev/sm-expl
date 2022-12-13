8bafbef28d2920970dc223d548d62b49d4a4acf6
Roman Holubenko <spiderr.roma@gmail.com>
2022-12-13 14:39:25 +0200

    check the validity of ticket

--- index.mjs
+++ index.mjs
@@ -4,10 +4,21 @@ import puppeteer from "puppeteer";
 // puppeteer.use(StealthPlugin());
 //# regex used [0-9a-z]{8}
 
-const dateConvert = (textDate) => {
-  const convertedDate = new Date(textDate);
+const dateConverter = (text) => {
+  const splitted = text.split(" ");
+  let date = splitted[1];
+  const time = splitted[2] + ":00";
+  date = date.replace(/\./g, "/");
+  date = date.substr(3, 2) + "/" + date.substr(0, 2) + "/" + date.substr(6, 4);
+  return new Date(new Date(date + " " + time)).toLocaleString({
+    timeZone: "Europe/Kyiv",
+  });
+};
+
+const dateCompare = (convertedDate) => {
   const today = new Date();
   return convertedDate >= today;
+  //Returns false if date is in the past, true if is future
 };
 
 const generateId = () => {
@@ -22,6 +33,7 @@ const generateId = () => {
 const randomUrl = `https://smartcinema.ua/payment-succeed/` + generateId();
 //static for trsting
 const url = `https://smartcinema.ua/payment-succeed/02e8914e`;
+//
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
@@ -31,18 +43,25 @@ const url = `https://smartcinema.ua/payment-succeed/02e8914e`;
     deviceScaleFactor: 1,
   });
   await page.goto(url);
-  // await autoScroll(page);
   let element = await page.$(".tickets-list");
-  //select and extract the date from span
-  let dataArray = await page.$eval(
-    // ".main .flex.column:last-child span.text-secondary",
-    "div:not([class])",
-    (el) => [el.innerText]);
-    const index = dataArray.indexOf("Вінниця");
-    // .map((el) => el.split(" "))
-  // let textDate = dataArray.find((el) => el[0] === "Дата");
-  console.log(dataArray, index);
-  dateConvert();
+  //Select and extract the date from span
+  let dataArray = await page.$eval("div:not([class])", (el) =>
+    el.innerText.replace(/(\r\n|\n|\r)/gm, " ").split(" ")
+  );
+  const index = dataArray.indexOf("Дата");
+  let day = dataArray[index + 1];
+  let date = dataArray[index + 2];
+  let time = dataArray[index + 3];
+  let textDate = day + " " + date + " " + time;
+
+  console.log(day, date, time);
+  const convertedDate = dateConverter(textDate);
+
+  console.log(dateConverter(textDate));
+
+  dateCompare(convertedDate);
+  console.log(dateCompare(convertedDate));
+
   element === null
     ? await browser.close
     : await page.screenshot({
